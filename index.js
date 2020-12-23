103ab6fe5e88dfd98f2c6bc62b5a5ebfc1e8942e
lzlmike <lzlli@ucdavis.edu>
2020-12-23 09:53:13 -0800

    added 0.1.21 support

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function adyenEncrypt(version) {
     version = ~~version;
 
-    if (version && (version >= 22 && version <= 25)) {
+    if (version && (version >= 21 && version <= 25)) {
         return require("./lib/0_1_" + version);
     }
 
-    return require("./lib/0_1_24");
+    return require("./lib/0_1_21");
 }
 
 module.exports = adyenEncrypt;
